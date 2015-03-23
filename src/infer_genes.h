e8ba50b73453d8552817a569d3681e141986e186
Vipin <vipin@cbio.mskcc.org>
2015-03-22 22:14:01 -0400

    fixed the header file for main program

---
+++ src/infer_genes.h
@@ -0,0 +1,47 @@
+#ifndef _INFER_GENES_H__
+#define _INFER_GENES_H__
+
+#include <string>
+	using std::string;
+#include <vector>
+	using std::vector;
+#include "region.h"
+#include "genome.h"
+#include "gene.h"
+#include "gene_tools.h"
+#include "Config.h"
+
+class InferGenes
+{
+	public: 
+		InferGenes(){conf = new Config();}; 
+		InferGenes(int argc, char *argv[]){conf = new Config(argc, argv);}; 
+		void infer_genes(Region* region, vector<Gene*>* genes);
+		int run_infer_genes();
+		Config* conf; 
+		vector<Region*> init_regions(bam_header_t* header);
+
+	private:
+		void process_gene(Gene* gene);
+		int find_next_intron(int idx, Region* region);
+		int find_previous_intron(int idx, Region* region, bool verb);
+		int score_cand_intron(int idx, Region* region);
+		int score_cand_exon(segment exon, Region* region);
+		segment find_terminal_exon(segment exon, Region* region);
+		segment find_initial_exon(segment exon, Region* region);
+		void find_intergenic_region(Region* region, Gene* gene);
+		vector<segment>* greedy_extend(int intron_idx, Region* region, bool* intron_used);
+		float mean(uint32_t* arr, int from, int to);
+		bool check_segment(segment seg, Region* region);
+		
+		//statistics: 
+		int no_upstream_intron; 
+		int no_upstream_intron_reject; 
+		int no_downstream_intron; 
+		int no_downstream_intron_reject; 
+		int init_reject_intron; 
+		int term_reject_intron;
+		int init_reject_cov;
+		int term_reject_cov;
+};
+#endif
